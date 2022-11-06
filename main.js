da2fc37cd2f6700de8a02ad53dc481a1ba8e4756
aleksanderwalczuk <ohleg@windowslive.com>
2022-11-06 17:21:25 +0100

    chore: init

---
+++ main.js
@@ -0,0 +1,16 @@
+import './src/scss/main.scss';
+import setupCounter from './src/counter';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+  </div>
+`;
+
+setupCounter(document.querySelector('#counter'));
