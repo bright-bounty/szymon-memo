56b149fac43c135df43c8b84c594637e666fde44
szymon <narek25512@gmail.com>
2022-11-23 19:34:41 +0100

    new modul added to main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import './src/scss/main.scss';
 import Header from './src/layout/Header';
+import ContentWrapper from './src/layout/ContentWrapper';
 
 const app = document.getElementById('app');
 app.appendChild(Header);
+
+const content = document.getElementById('content');
+content.appendChild(ContentWrapper);
---
+++ src/layout/ContentWrapper.js
@@ -0,0 +1,5 @@
+function ContentWrapper() {
+  const contentWrapper = document.createElement('ContentWrapper');
+  return contentWrapper;
+}
+export default ContentWrapper;
