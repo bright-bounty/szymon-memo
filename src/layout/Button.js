99ca10b11d7a518771c3143924e740ab3b6b9ffc
szymon <narek25512@gmail.com>
2022-11-24 20:15:13 +0100

    add new componentnt Button.js

---
+++ src/layout/Button.js
@@ -0,0 +1,7 @@
+function Btn(text) {
+  const element = document.createElement('button');
+  element.textContent = text;
+  // const button = ??? ;
+  return element;
+}
+export default Btn;
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,5 +1,8 @@
+import Btn from './Button';
+
 function Header() {
   const header = document.createElement('header');
+  header.appendChild(Btn);
   return header;
 }
 export default Header;
