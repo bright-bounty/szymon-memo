81ac1f2bb665494947d1c31c716f594e3e053182
szymon <narek25512@gmail.com>
2022-11-27 11:19:46 +0100

    merge

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './src/scss/main.scss';
 import Header from './src/layout/Header';
 import ContentWrapper from './src/layout/ContentWrapper';
 
+const header = Header();
+const wrapper = ContentWrapper();
 const app = document.getElementById('app');
-app.appendChild(Header);
-app.appendChild(ContentWrapper);
+app.appendChild(header);
+app.appendChild(wrapper);
--- src/layout/Button.js
+++ src/layout/Button.js
@@ -1,6 +1,7 @@
 function Btn(text) {
   const element = document.createElement('button');
   element.textContent = text;
+  element.onclick = Btn;
   return element;
 }
 export default Btn;
